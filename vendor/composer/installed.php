b41a77513148bd25bf683f78fe3feb7e61c87c71
juan.tabares <juan.tabares@coytex.com.co>
2021-07-03 18:51:11 -0500

    Libreria PDF

---
+++ vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '1b99eb0685bf9bb499bac14bceab1700da96ce99',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '1b99eb0685bf9bb499bac14bceab1700da96ce99',
+    ),
+    'fpdf/fpdf' => 
+    array (
+      'pretty_version' => '1.83.1',
+      'version' => '1.83.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '9a27abfd0a8e72ef690e94f3d5efc862541a3ff2',
+    ),
+  ),
+);
