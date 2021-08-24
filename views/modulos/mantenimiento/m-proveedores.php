2c8795414ce070a5c7059ccebee006685752ef34
Mateo <matte_sr97@hotmail.com>
2021-08-24 17:27:39 -0500

    Creacion vista mantenimiento proveedores

---
+++ views/modulos/mantenimiento/m-proveedores.php
@@ -0,0 +1,51 @@
+<?php
+
+// if (!validarModulo('CARGAR_OPCION')) {
+//     echo "<script> window.location = 'inicio'; </script>";
+// }
+
+
+?>
+<!-- ===================== 
+  MODELO PARA LA IMPLEMENTARCION EN EL DISEÑO DE LOS MODULOS
+  ESTRUCTURA 
+========================= -->
+
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1 class="m-0 text-dark ">Plantilla</h1>
+                </div><!-- /.col -->
+                <div class="col-sm-6">
+                    <ol class="breadcrumb float-sm-right">
+                        <li class="breadcrumb-item"><a href="inicio">Inicio</a></li>
+                        <li class="breadcrumb-item active">Plantilla</li>
+                    </ol>
+                </div><!-- /.col -->
+            </div><!-- /.row -->
+        </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content-header -->
+
+    <!-- Main content -->
+    <div class="content">
+        <div class="container-fluid">
+            <!-- ===================== 
+              AGREGAR FILAS Y COLUMNAS PARA EL DESARROLLO 
+            ========================= -->
+            <div class="row">
+                <div class="col-md-12  card ">
+
+                </div><!-- col -->
+
+
+            </div> <!-- /.row -->
+        </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content -->
+</div>
+<!-- /.content-wrapper -->
