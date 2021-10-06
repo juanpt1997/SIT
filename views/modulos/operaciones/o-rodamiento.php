3c074d2611113df7199f80071a884182cac6ce5d
juan.tabares <juan.tabares@coytex.com.co>
2021-10-06 17:46:26 -0500

    inputs en rodamiento

--- views/modulos/operaciones/o-rodamiento.php
+++ views/modulos/operaciones/o-rodamiento.php
@@ -166,6 +166,12 @@
                             </div>
                         </div>
 
+                        <!-- ******* TIPO DE VINCULACION ******* -->
+                        <!-- ******* CONDUCTOR ******* -->
+                        <!-- ******* CLIENTE ******* -->
+                        <!-- ******* TIPO DE SERVICIO ******* -->
+
+
                         <div class="col-12 col-sm-6 col-lg-4">
                             <div class="form-group">
                                 <label>Fecha de Servicio</label>
