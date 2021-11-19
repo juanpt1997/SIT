3933e00910e05bf9a0d2c0af94c172a87a151539
unknown <gironsteven75@gmail.com>
2021-11-19 17:55:38 -0500

    agregar producto, añadir inventario y generar movimiento

--- models/almacen.modelo.php
+++ models/almacen.modelo.php
@@ -7,24 +7,24 @@ class ModeloProductos
 
     static public function mdlAgregarProducto($datos)
     {
-        $stmt = Conexion::conectar()->prepare("INSERT INTO a_productos (codigo,referencia,descripcion,idcategoria,idmarca,idmedida) VALUES (:codigo,:referencia,:descripcion,:idcategoria,:idmarca,:idmedida)");
-        $stmt->bindParam(":codigo", $datos["codigo"], PDO::PARAM_STR);
+        $conexion = Conexion::conectar();
+        $stmt = $conexion->prepare("INSERT INTO a_productos (codigo,referencia,descripcion,idcategoria,idmarca,idmedida) VALUES (:codigo,:referencia,:descripcion,:idcategoria,:idmarca,:idmedida)");
+        $stmt->bindParam(":codigo", $datos["cod_producto"], PDO::PARAM_STR);
         $stmt->bindParam(":referencia", $datos["referencia"], PDO::PARAM_INT);
-        $stmt->bindParam(":descripcion", $datos["descripcion"], PDO::PARAM_STR);
-        $stmt->bindParam(":idcategoria", $datos["idcategoria"], PDO::PARAM_INT);
-        $stmt->bindParam(":idmarca", $datos["idmarca"], PDO::PARAM_INT);
-        $stmt->bindParam(":idmedida", $datos["idmedida"], PDO::PARAM_INT);
+        $stmt->bindParam(":descripcion", $datos["descripcion_prod"], PDO::PARAM_STR);
+        $stmt->bindParam(":idcategoria", $datos["categoria"], PDO::PARAM_INT);
+        $stmt->bindParam(":idmarca", $datos["marca"], PDO::PARAM_INT);
+        $stmt->bindParam(":idmedida", $datos["medida"], PDO::PARAM_INT);
 
         if ($stmt->execute()) {
-            $retorno = "ok";
+            $id = $conexion->lastInsertId();
         } else {
-            $retorno = "error";
+            $id = "error";
         }
 
         $stmt->closeCursor();
         $stmt = null;
-
-        return $retorno;
+        return $id;
     }
 
     static public function mdlEditarProducto($datos)
@@ -80,4 +80,64 @@ class ModeloProductos
         $stmt->closeCursor();
         return $retorno;
     }
+
+    static public function mdlDatosProducto($datos)
+    {
+        $stmt = Conexion::conectar()->prepare("SELECT p.*, c.categoria AS categoria, ma.marca AS marca, me.medida AS medida FROM a_productos p
+        INNER JOIN a_categorias c ON p.idcategoria = c.idcategorias
+        INNER JOIN a_marcas ma ON p.idmarca = ma.idmarca
+        INNER JOIN a_medidas me ON p.idmedida = me.idmedidas
+        WHERE p.{$datos['item']} = :{$datos['item']}");
+
+        $stmt->bindParam(":{$datos['item']}", $datos['valor']);
+        $stmt->execute();
+        $retorno =  $stmt->fetch();
+
+        $stmt->closeCursor();
+        return $retorno;
+    }
+
+    static public function mdlAgregarInventario($datos)
+    {
+        $conexion = Conexion::conectar();
+        $stmt = $conexion->prepare("INSERT INTO a_re_inventario (idproducto,idsucursal,stock) VALUES (:idproducto,:idsucursal,:stock)");
+        $stmt->bindParam(":idproducto", $datos["idproducto"], PDO::PARAM_INT);
+        $stmt->bindParam(":idsucursal", $datos["idsucursal"], PDO::PARAM_INT);
+        $stmt->bindParam(":stock", $datos["stock"], PDO::PARAM_INT);
+
+        if ($stmt->execute()) {
+            $id = $conexion->lastInsertId();
+        } else {
+            $id = "error";
+        }
+        $stmt->closeCursor();
+        $stmt = null;
+        return $id;
+    }
+
+    static public function mdlAgregarMovimiento($datos)
+    {
+        $stmt = Conexion::conectar()->prepare("INSERT INTO a_re_movimientoinven(idinventario,cantidad,tipo_movimiento,preciocompra,idproveedor,facturacompra)
+                                                VALUES(:idinventario,:cantidad,:tipo_movimiento,:preciocompra,:idproveedor,:facturacompra)");
+
+        $stmt->bindParam(":idinventario", $datos["idinventario"], PDO::PARAM_INT);
+        $stmt->bindParam(":cantidad", $datos["cantidad"], PDO::PARAM_INT);
+        $stmt->bindParam(":tipo_movimiento", $datos["tipo_movimiento"], PDO::PARAM_STR);
+        $stmt->bindParam(":preciocompra", $datos["preciocompra"], PDO::PARAM_INT);
+        $stmt->bindParam(":idproveedor", $datos["idproveedor"], PDO::PARAM_INT);
+        $stmt->bindParam(":facturacompra", $datos["facturacompra"], PDO::PARAM_STR);
+
+        if ($stmt->execute()) {
+            $retorno = "ok";
+        } else {
+            $retorno = "error";
+        }
+
+        $stmt->closeCursor();
+        $stmt = null;
+
+        return $retorno;
+    }
+
+
 }
