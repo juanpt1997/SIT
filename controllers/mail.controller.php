94e2719f52e03616f854e6049b7a8db7fea3b475
juan.tabares <juan.tabares@coytex.com.co>
2022-01-14 15:36:03 -0500

    correo origen modificado

--- controllers/mail.controller.php
+++ controllers/mail.controller.php
@@ -14,7 +14,7 @@ use PHPMailer\PHPMailer\Exception; */
     {
         static public function ctrEnviarCorreo($to, $subject, $message)
         {
-            $headers = "From: tecnolab@tecnolab.com.co" . "\r\n";
+            $headers = "From: no-responder@tecnolab.com.co" . "\r\n";
             $headers .= "MIME-Version: 1.0" . "\r\n";
             $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
             mail($to, $subject, $message, $headers);
